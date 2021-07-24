c90240bc920ddbc74441a6caf5fa1b3636286591
Michal Rostecki <mrostecki@opensuse.org>
2021-07-24 11:58:17 +0200

    guestfs: Add Meson, Change from TW to Leap, suppoer custom kernels
    This commit contains the following changes to our guestfs infra:
    
    - Add Meson, which is now used as our build system
    - Switch from openSUSE Tumbleweed to Leap with some packages (especially
      the kernel) installed from custom repos
    - Optionally install the custom kernel from the source tree - that
      source tree has to be build on the host already.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- contrib/guestfs/build.sh
+++ contrib/guestfs/build.sh
@@ -2,12 +2,18 @@
 
 # This script builds the base qcow2 image for lockc development environment.
 
-BASE_IMAGE="openSUSE-Tumbleweed-JeOS.qcow2"
+set -e
+
+LEAP_VERSION="15.3"
+BASE_IMAGE="openSUSE-Leap-${LEAP_VERSION}-JeOS.qcow2"
 LOCKC_IMAGE="lockc-base.qcow2"
 
-if [ ! -f ${BASE_IMAGE} ]; then
+CUSTOM_KERNEL=${CUSTOM_KERNEL:-false}
+KERNEL_SOURCE=${KERNEL_SOURCE:-${HOME}/linux}
+
+if [[ ! -f ${BASE_IMAGE} ]]; then
     wget -O ${BASE_IMAGE} \
-        https://download.opensuse.org/tumbleweed/appliances/openSUSE-Tumbleweed-JeOS.x86_64-OpenStack-Cloud.qcow2
+        http://download.opensuse.org/distribution/leap/15.3/appliances/openSUSE-Leap-15.3-JeOS.x86_64-OpenStack-Cloud.qcow2
 fi
 
 rm -f ${LOCKC_IMAGE}
@@ -15,14 +21,23 @@ cp ${BASE_IMAGE} ${LOCKC_IMAGE}
 qemu-img resize ${LOCKC_IMAGE} 40G
 virt-resize --expand /dev/sda3 ${BASE_IMAGE} ${LOCKC_IMAGE}
 
-virt-customize -v -x -a \
+EXTRA_FLAGS=""
+
+if [[ "${CUSTOM_KERNEL}" == "true" ]]; then
+    echo "Building with a custom kernel source tree ${KERNEL_SOURCE}"
+
+    EXTRA_FLAGS+="--copy-in ${KERNEL_SOURCE}:/usr/src/"
+fi
+
+set -eux
+
+virt-customize -a \
     ${LOCKC_IMAGE} \
     --mkdir /etc/containerd \
     --mkdir /etc/docker \
     --copy-in ../etc/modules-load.d/99-k8s.conf:/etc/modules-load.d/ \
     --copy-in ../etc/sysctl.d/99-k8s.conf:/etc/sysctl.d/ \
-    --copy-in ../systemd/lockcd.service:/etc/systemd/system/ \
-    --copy-in ../etc/containerd/config.toml:/etc/containerd/ \
-    --copy-in ../etc/docker/daemon.json:/etc/docker/ \
     --copy-in ../systemd/containerd.service:/etc/systemd/system/ \
-    --run provision.sh
+    --copy-in install-lockc.sh:/usr/sbin/ \
+    ${EXTRA_FLAGS} \
+    --run provision/provision.sh
