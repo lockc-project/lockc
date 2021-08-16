1f3be84596f07f03255bb42084a484c73fc3b4f3
Michal Rostecki <mrostecki@opensuse.org>
2021-08-16 23:00:59 +0200

    meson
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -6,9 +6,18 @@ export OUTPUT1="$2"
 export OUTPUT2="$3"
 export CARGO_TARGET_DIR="$4"/target
 export CARGO_HOME="${CARGO_TARGET_DIR}"/cargo-home
+export PROFILE="$5"
 
-cargo build --manifest-path $1/Cargo.toml --release -p lockc
+EXTRA_FLAGS="--release"
+CARGO_TARGET_SUBDIR="release"
+
+if [[ "${PROFILE}" == "dev" ]]; then
+    EXTRA_FLAGS=""
+    CARGO_TARGET_SUBDIR="debug"
+fi
+
+cargo build --manifest-path $1/Cargo.toml ${EXTRA_FLAGS} -p lockc
 
 for out in "${OUTPUT1}" "${OUTPUT2}"; do
-    cp "${CARGO_TARGET_DIR}"/release/$(basename "${out}") "${out}"
+    cp "${CARGO_TARGET_DIR}"/${CARGO_TARGET_SUBDIR}/$(basename "${out}") "${out}"
 done
