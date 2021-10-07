b3aecbd921a2b69301ddefd464f6cb6b417d045f
Michal Rostecki <mrostecki@opensuse.org>
2021-10-07 15:25:21 +0200

    scripts: Remove leftover scripts after meson
    We don't use meson anymore and the purpose of those scripts was only to
    glue meson with cargo and rest of tools we need for build.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- scripts/libbpf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-CC=${CC:-"$1"}
-
-cargo libbpf build --clang-path "${CC}"
-cargo libbpf gen
--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-export CARGO_TARGET_DIR="$1/target/"
-export CARGO_HOME="$CARGO_TARGET_DIR/cargo-home"
-
-cargo test
