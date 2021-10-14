277a7418850f79d6d68ee15321c343cd17a3654f
Michal Jura <mjura@suse.com>
2021-10-14 18:26:50 +0200

    Add 9p drivers to initrd
    Guestfs image needs to have 9p 9pnet 9pnet-virtio drivers included in initrd.
    This has to be added to dracut during guestfs image provisioning.
    
    Signed-off-by: Michal Jura <mjura@suse.com>

--- contrib/guestfs/provision/provision.sh
+++ contrib/guestfs/provision/provision.sh
@@ -75,6 +75,15 @@ sed -i -e "s/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"lsm=bpf,integrity\"/" \
     /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
+### Add 9p drivers to dracut
+cat >> /etc/dracut.conf.d/90-9p.conf << EOF
+# Add 9p 9pnet and 9pnet_virtio modules
+add_drivers+=" 9p 9pnet 9pnet_virtio "
+EOF
+
+### Rebuild initrd with dracut
+mkinitrd
+
 mv /etc/containerd/config.toml.rpmorig /etc/containerd/config.toml
 
 systemctl enable containerd
