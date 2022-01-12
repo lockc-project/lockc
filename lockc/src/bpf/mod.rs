526d3a14aeb165700d87d668cc5bb4ae5338e3f2
Michal Rostecki <mrostecki@opensuse.org>
2022-01-12 14:54:53 +0100

    bpf: Stop using bpf/ dir as a Rust module
    Since we are dropping the usage of libbpf-rs, we are going to keep only
    C source files (to be built directly with Clang).
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- lockc/src/bpf/mod.rs
+++
@@ -1,6 +0,0 @@
-#![allow(dead_code)]
-#![allow(non_snake_case)]
-#![allow(non_camel_case_types)]
-#![allow(clippy::transmute_ptr_to_ref)]
-#![allow(clippy::upper_case_acronyms)]
-include!(concat!(env!("OUT_DIR"), "/lockc.skel.rs"));
